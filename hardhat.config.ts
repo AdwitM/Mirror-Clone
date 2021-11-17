5a785ea3cf9c10e3e9f98541358f6e4111fd1d2a
Bartosz Hejman <bartoszhejman@gmail.com>
2021-11-17 15:11:12 +0100

    Fix unit tests

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,7 +6,7 @@ import '@nomiclabs/hardhat-waffle';
 import '@typechain/hardhat';
 import 'hardhat-gas-reporter';
 import 'solidity-coverage';
-import path from "path";
+import path from 'path';
 
 dotenv.config({path: path.resolve(process.cwd(), '.env.local')});
 
--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,31 @@
 module.exports = {
+  roots: ['<rootDir>'],
+  testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/__tests__/setup.js'],
-  testPathIgnorePatterns: ['setup.js', 'utils'],
-  modulePaths: ['node_modules', '<rootDir>'],
+  testPathIgnorePatterns: [
+    '<rootDir>/web3',
+    '<rootDir>/node_modules/',
+    '<rootDir>/.next/',
+    'setup.js',
+    'utils',
+  ],
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', {presets: ['next/babel']}],
+  },
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname',
+  ],
   moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
     '@/typechain': '<rootDir>/typechain',
-    '@/pages': '<rootDir>/pages',
+    '@/pages/(.*)': '<rootDir>/pages/$1',
     '@/components': '<rootDir>/components',
     '@/components/(.*)': '<rootDir>/components/$1',
   },
-  modulePathIgnorePatterns: ['<rootDir>/web3'],
 };
