727464a3908d4b5ef82de47b5aec7eeed64d023d
Bartosz Hejman <bartoszhejman@gmail.com>
2021-12-02 11:06:05 +0100

    Add comments

--- pages/api/arweave/search/[[...query]].ts
+++ pages/api/arweave/search/[[...query]].ts
@@ -23,15 +23,23 @@ export default async function (
 ): Promise<any> {
   try {
     const {query} = _req.query;
+
+    // Retrieve searchAddress
     const searchAddress = query && query[0];
+
+    // Initialize ArDB
+    // More information about ArDB can be found here: https://www.npmjs.com/package/ardb
     const ardb = new ArDB(arweave);
 
+    // Build tags
     const tags = [{name: 'App-Name', values: [process.env.APP_NAME as string]}];
 
     if (searchAddress) {
       tags.push({name: 'Address', values: [searchAddress]});
     }
 
+    // Search for transaction withs App-Name and Address (optional) tags
+    // More information can be found here: https://www.npmjs.com/package/ardb
     const txs = await ardb.search('transactions').tags(tags).limit(10).find();
 
     const promises = txs.map((tx: any) => getData(tx._id));
