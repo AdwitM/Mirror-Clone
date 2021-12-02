329f0754e356bade3ebf84772385c877841fd92d
Bartosz Hejman <bartoszhejman@gmail.com>
2021-12-02 11:10:17 +0100

    Add comments

--- pages/api/arweave/[transactionHash].ts
+++ pages/api/arweave/[transactionHash].ts
@@ -14,6 +14,7 @@ export default async function (
   try {
     const {transactionHash} = req.query;
 
+    // Get Arweave transaction data. Documentation can be found here: https://github.com/ArweaveTeam/arweave-js
     const txDataResp = (await arweave.transactions.getData(
       transactionHash as string,
       {
@@ -23,6 +24,7 @@ export default async function (
     )) as string;
     const txData = JSON.parse(txDataResp);
 
+    // Get Arweave transaction status. Documentation can be found here: https://github.com/ArweaveTeam/arweave-js
     const txStatusResp = await arweave.transactions.getStatus(
       transactionHash as string,
     );
@@ -37,18 +39,29 @@ export default async function (
 
     // We should show only confirmed transactions to avoid sniffing and claiming NFT
     if (txStatus === TransactionStatusE.CONFIRMED) {
+      // Get Arweave transaction block in order to retrieve timestamp. Documentation can be found here: https://github.com/ArweaveTeam/arweave-js
       const block = txStatusResp.confirmed
         ? await arweave.blocks.get(txStatusResp.confirmed.block_indep_hash)
         : null;
 
+      // Get Arweave transaction. Documentation can be found here: https://github.com/ArweaveTeam/arweave-js
       const tx = await arweave.transactions.get(transactionHash as string);
 
+      // Get Arweave transaction tags. Documentation can be found here: https://github.com/ArweaveTeam/arweave-js
       const tags = {} as PostTagsT;
       (tx.get('tags') as any).forEach((tag) => {
         const key = tag.get('name', {decode: true, string: true});
         tags[key] = tag.get('value', {decode: true, string: true});
       });
 
+      // Return JSON response in form:
+      // res.status(200).json({
+      //   id: transactionHash as string,
+      //   data: txData,
+      //   status: txStatus,
+      //   timestamp: block?.timestamp,
+      //   tags,
+      // });
       res.status(200).json({
         id: transactionHash as string,
         data: txData,
